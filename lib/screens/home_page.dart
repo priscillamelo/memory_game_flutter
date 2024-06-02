ea9e08fcdba6d0a272ab4b935e55e45aae6752b0
priscillamelo <priscillamelo73@gmail.com>
2024-06-02 17:59:20 -0300

    screens--home-page--recordes-page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../values/theme/round6_theme.dart';
 import '../widgets/button.dart';
+import '../widgets/card_recordes.dart';
 import '../widgets/logo_widget.dart';
 
 class HomePage extends StatelessWidget {
@@ -12,38 +13,40 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const LogoWidget(),
-            Button(
-                title: 'Modo Normal',
-                colorButton: Colors.white,
-                actionButton:
-                    () {} /* Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (BuildContext context) =>
-                      RecordesPage(modo: Modo.normal),
-                ),
-              ), */
-                ),
-            Button(
-                title: 'Modo Round 6',
-                colorButton: Round6Theme.colorPrimary,
-                actionButton:
-                    () {} /* Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (BuildContext context) =>
-                      RecordesPage(modo: Modo.normal),
-                ),
-              ), */
-                ),
-            //const SizedBox(height: 60),
-            //const CardRecordes(),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const LogoWidget(),
+              Button(
+                  title: 'Modo Normal',
+                  colorButton: Colors.white,
+                  actionButton:
+                      () {} /* Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        RecordesPage(modo: Modo.normal),
+                  ),
+                ), */
+                  ),
+              Button(
+                  title: 'Modo Round 6',
+                  colorButton: Round6Theme.colorPrimary,
+                  actionButton:
+                      () {} /* Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) =>
+                        RecordesPage(modo: Modo.normal),
+                  ),
+                ), */
+                  ),
+              const SizedBox(height: 60),
+              const CardRecordes(),
+            ],
+          ),
         ),
       ),
     );
